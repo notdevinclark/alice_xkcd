cf4985b2c9967542db3c7e2aa96f44a03c65a370
Devin Clark <notdevinclark@gmail.com>
2017-08-20 13:42:50 -0700

    updates version number to 0.0.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AliceXkcd.Mixfile do
 
   def project do
     [app: :alice_xkcd,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
